cf588e8b691987b1ce392fc0369b477aafa2adb2
99061142 <99061142@mydavinci.nl>
2021-12-12 00:25:31 +0100

    Code to make 3 buttons, and use the input of the user to change the number for the second button

---
+++ buttons.py
@@ -0,0 +1,69 @@
+import tkinter
+
+
+window = tkinter.Tk() # Makes the window
+window.title("Clicker") # Window title
+window.configure(bg="gray") # Window background color
+
+
+global button_with_num # Button with the number that changes
+
+
+# Change the number when the user clicks on 'up' or 'down'
+def change_num(text):
+    button_with_num['text'] += 1 if text == "up" else -1 # Change number
+
+    # Change the window background
+    if button_with_num['text'] < 0:
+        window.configure(bg="red")
+    elif button_with_num['text'] > 0:
+        window.configure(bg="green")
+    else:
+        window.configure(bg="gray")
+
+
+# Make the 3 buttons
+def make_buttons():
+    for button_num in range(1, 4):
+        button = tkinter.Button(window, font=("arial", 10, 'bold'), width=20)
+
+
+        button.pack(
+            fill='both',
+            expand=True,
+            pady=20,
+            padx=20
+        )
+
+        # If its the first button
+        if button_num == 1: 
+            # Button text
+            text = "up"    
+            button['text'] = text
+
+            button['command'] = lambda para=text: change_num(para) # Function to change the number
+
+        # If its the second button
+        elif button_num == 2:
+            # Use the button with the number for the global variable
+            global button_with_num 
+            button_with_num = button
+
+            button['text'] = 0 # Button text
+        
+        # If its the third button
+        else:
+            # Button text
+            text = "down"
+            button['text'] = text
+
+            button['command'] = lambda para=text: change_num(para) # Function to change the number
+
+
+        button.pack() # Add the button to the window
+
+
+# If the code starts
+if __name__ == "__main__":
+    make_buttons() # Makes the 3 buttons
+    window.mainloop() # Open the window
